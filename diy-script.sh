3b3f54886a1416acdc18578f24e32bb81ac7bb30
zhugunzi <zhugunzi@gmail.com>
2023-01-09 21:54:21 +0800

    Update diy-script.sh
    mmmm

--- diy-script.sh
+++ diy-script.sh
@@ -8,7 +8,7 @@
 #===============================================
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Change default shell to zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
